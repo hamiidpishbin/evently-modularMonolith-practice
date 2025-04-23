546362c50703ce4f4390abd1cb8d97a555d569c0
Hamid Pishbin <61520042+hamiidpishbin@users.noreply.github.com>
2025-04-23 18:12:08 +0330

    Add HealthChecks

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -3,6 +3,8 @@ using Evently.Api.Middleware;
 using Evently.Common.Application;
 using Evently.Common.Infrastructure;
 using Evently.Modules.Events.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,9 +23,14 @@ builder.Configuration.AddModuleConfigurations(["events"]);
 
 builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
 
-builder.Services.AddInfrastructure(
-	builder.Configuration.GetConnectionString("Database")!,
-	builder.Configuration.GetConnectionString("redis")!); 
+var databaseConnectionString = builder.Configuration.GetConnectionString("Database")!;
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis")!;
+
+builder.Services.AddInfrastructure(databaseConnectionString, redisConnectionString);
+
+builder.Services.AddHealthChecks()
+	.AddNpgSql(databaseConnectionString)
+	.AddRedis(redisConnectionString);
 
 builder.Services.AddEventsModule(builder.Configuration);
 
@@ -34,13 +41,19 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-	
+
 	app.ApplyMigrations();
 }
 
 app.MapGet("/", () => "Hello World!");
 
 EventsModule.MapEndpoints(app);
+
+app.MapHealthChecks("health",
+	new HealthCheckOptions()
+	{
+		ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+	});
 
 app.UseHttpsRedirection();
 
